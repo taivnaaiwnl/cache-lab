e95d5fe7d382d40a54090172c16afeff1ae481d8
taivnaaiwnl <taivnaaiwnl@gmail.com>
2025-03-04 21:14:18 +0800

    add test

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,225 @@
 #include "cachelab.h"
-//22B1NUM6769 Taivanbat Bayarsaikhan 
-int main()
-{
-    printSummary(0, 0, 0);
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+    bool valid;
+    unsigned long long tag;
+    int lru_counter;
+} cache_line_t;
+
+typedef struct {
+    int s; // set index bits
+    int E; // Associativity
+    int b; // block bits
+    cache_line_t** lines;
+} cache_t;
+
+void usage(char* argv[]);
+void init_cache(cache_t* cache, int s, int E, int b);
+void free_cache(cache_t* cache, int S);
+void process_trace_file(cache_t* cache, char* trace_file, bool verbose, int* hits, int* misses, int* evictions);
+bool access_cache(cache_t* cache, unsigned long long address, int* hits, int* misses, int* evictions);
+
+int main(int argc, char* argv[]) {
+    int opt;
+    char* trace_file = NULL;
+    bool verbose = false;
+    int s = 0, E = 0, b = 0;
+    int hits = 0, misses = 0, evictions = 0;
+    
+    while ((opt = getopt(argc, argv, "vs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'v':
+                verbose = true;
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                trace_file = optarg;
+                break;
+            default:
+                usage(argv);
+                exit(EXIT_FAILURE);
+        }
+    }
+    
+    if (s <= 0 || E <= 0 || b <= 0 || trace_file == NULL) {
+        usage(argv);
+        exit(EXIT_FAILURE);
+    }
+    
+    cache_t cache;
+    init_cache(&cache, s, E, b);
+    
+    process_trace_file(&cache, trace_file, verbose, &hits, &misses, &evictions);
+    
+    printSummary(hits, misses, evictions);
+    
+    int S = 1 << s; // sets
+    free_cache(&cache, S);
+    
     return 0;
+}
+
+void usage(char* argv[]) {
+    printf("Usage: %s [-v] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0]);
+    printf("  -v: Optional verbose flag\n");
+    printf("  -s <s>: Number of set index bits\n");
+    printf("  -E <E>: Associativity (number of lines per set)\n");
+    printf("  -b <b>: Number of block bits\n");
+    printf("  -t <tracefile>: Name of the trace file\n");
+}
+
+void init_cache(cache_t* cache, int s, int E, int b) {
+    int S = 1 << s; //sets
+    
+    cache->s = s;
+    cache->E = E;
+    cache->b = b;
+    
+    cache->lines = (cache_line_t**)malloc(S * sizeof(cache_line_t*));
+    for (int i = 0; i < S; i++) {
+        cache->lines[i] = (cache_line_t*)malloc(E * sizeof(cache_line_t));
+        for (int j = 0; j < E; j++) {
+            cache->lines[i][j].valid = false;
+            cache->lines[i][j].tag = 0;
+            cache->lines[i][j].lru_counter = 0;
+        }
+    }
+}
+
+void free_cache(cache_t* cache, int S) {
+    for (int i = 0; i < S; i++) {
+        free(cache->lines[i]);
+    }
+    free(cache->lines);
+}
+
+void process_trace_file(cache_t* cache, char* trace_file, bool verbose, int* hits, int* misses, int* evictions) {
+    FILE* fp = fopen(trace_file, "r");
+    if (fp == NULL) {
+        printf("Error: Cannot open trace file %s\n", trace_file);
+        exit(EXIT_FAILURE);
+    }
+    
+    char operation;
+    unsigned long long address;
+    int size;
+    char buffer[100];
+    
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (buffer[0] == 'I' || buffer[0] == '\n') {
+            continue;
+        }
+        
+        sscanf(buffer, " %c %llx,%d", &operation, &address, &size);
+        
+        if (verbose) {
+            printf("%c %llx,%d ", operation, address, size);
+        }
+        
+        bool hit = access_cache(cache, address, hits, misses, evictions);
+        
+        if (verbose) {
+            if (hit) {
+                printf("hit ");
+            } else {
+                printf("miss ");
+            }
+        }
+        
+        if (operation == 'M') {
+            hit = access_cache(cache, address, hits, misses, evictions);
+            if (verbose) {
+                if (hit) {
+                    printf("hit ");
+                } else {
+                    printf("miss ");
+                }
+            }
+        }
+        
+        if (verbose) {
+            printf("\n");
+        }
+    }
+    
+    fclose(fp);
+}
+
+bool access_cache(cache_t* cache, unsigned long long address, int* hits, int* misses, int* evictions) {
+    int s = cache->s;
+    int b = cache->b;
+    int E = cache->E;
+    
+    unsigned long long set_index = (address >> b) & ((1 << s) - 1);
+    unsigned long long tag = address >> (s + b);
+    
+    int max_lru = -1;
+    int max_lru_index = -1;
+    int empty_line = -1;
+    
+    for (int i = 0; i < E; i++) {
+        if (cache->lines[set_index][i].valid) {
+            if (cache->lines[set_index][i].lru_counter > max_lru) {
+                max_lru = cache->lines[set_index][i].lru_counter;
+                max_lru_index = i;
+            }
+            
+            if (cache->lines[set_index][i].tag == tag) {
+                (*hits)++;
+                cache->lines[set_index][i].lru_counter = 0;
+                
+                for (int j = 0; j < E; j++) {
+                    if (cache->lines[set_index][j].valid && j != i) {
+                        cache->lines[set_index][j].lru_counter++;
+                    }
+                }
+                
+                return true; // Hit
+            }
+        } else {
+            if (empty_line == -1) {
+                empty_line = i;
+            }
+        }
+    }
+    
+    (*misses)++;
+    
+    if (empty_line != -1) {
+        cache->lines[set_index][empty_line].valid = true;
+        cache->lines[set_index][empty_line].tag = tag;
+        cache->lines[set_index][empty_line].lru_counter = 0;
+        
+        for (int j = 0; j < E; j++) {
+            if (cache->lines[set_index][j].valid && j != empty_line) {
+                cache->lines[set_index][j].lru_counter++;
+            }
+        }
+    } else {
+        (*evictions)++;
+        cache->lines[set_index][max_lru_index].tag = tag;
+        cache->lines[set_index][max_lru_index].lru_counter = 0;
+        
+        for (int j = 0; j < E; j++) {
+            if (j != max_lru_index) {
+                cache->lines[set_index][j].lru_counter++;
+            }
+        }
+    }
+    
+    return false; // Miss
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -14,9 +14,6 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-void transpose_32bit(int M, int N, int A[N][M], int B[M][N]);
-void transpose_64bit(int M, int N, int A[N][M], int B[M][N]);
-void transpose_61bit(int M, int N, int A[N][M], int B[M][N]);
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -25,50 +22,120 @@ void transpose_61bit(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-    if (M == 32 && N == 32) 
-        transpose_32bit(M,N,A,B)
-    if (M == 64 && N == 64)
-        transpose_64bit(M,N,A,B)
-    if (M==61 && N == 67)
-        transpose_61bit(M,N,A,B)
-}
-void transpose_32bit(int M, int N, int A[N][M], int B[M][N]){
-    for (int i = 0; i <= 32; i++)
-        for (int j = 0; j <= 32; j++)
-            for(int k = i; k <= i+8; k++)
-        {
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]);
 
+void transpose_32bit(int M, int N, int A[N][M], int B[M][N]);
+void transpose_64bit(int M, int N, int A[N][M], int B[M][N]);
+void transpose_61bit(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    if (M == 32 && N == 32) {
+        transpose_32bit(M, N, A, B);
+    } else if (M == 64 && N == 64) {
+        transpose_64bit(M, N, A, B);
+    } else if (M == 61 && N == 67) {
+        transpose_61bit(M, N, A, B);
+    } else {
+        int i, j, tmp;
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < M; j++) {
+                tmp = A[i][j];
+                B[j][i] = tmp;
+            }
         }
+    }
+}
 
+void transpose_32bit(int M, int N, int A[N][M], int B[M][N]) {
+    int i, j, ii, jj;
+    int tmp;
+    int block_size = 8;
     
-}
-void transpose_64bit(int M, int N, int A[N][M], int B[M][N]){
-    for (int i = 0; i <= 64; i += 8)
-        for(int j = 0; j <= 64; j += 8)
-            for(int k = i; k <= i + 4; k++)
-            {
-                
+    for (ii = 0; ii < N; ii += block_size) {
+        for (jj = 0; jj < M; jj += block_size) {
+            for (i = ii; i < ii + block_size && i < N; i++) {
+                for (j = jj; j < jj + block_size && j < M; j++) {
+                    if (i != j) {
+                        B[j][i] = A[i][j];
+                    } else {
+                        tmp = A[i][j];
+                        B[j][i] = tmp;
+                    }
+                }
             }
-    
+        }
+    }
 }
-void transpose_61bit(int M, int N, int A[N][M], int B[M][N]){
-    for (int i = 0; i <= 61; i += 8)
-        for (int j = 0; j <= 67; j += 8)
-            for (int k = i; k <= i + 3; k++)
-            {
-                
-            }
+
+void transpose_64bit(int M, int N, int A[N][M], int B[M][N]) {
+    int i, j, k;
+    int v0, v1, v2, v3, v4, v5, v6, v7;
     
+    for (i = 0; i < N; i += 8) {
+        for (j = 0; j < M; j += 8) {
+            for (k = 0; k < 4; k++) {
+                v0 = A[i+k][j];
+                v1 = A[i+k][j+1];
+                v2 = A[i+k][j+2];
+                v3 = A[i+k][j+3];
+                v4 = A[i+k][j+4];
+                v5 = A[i+k][j+5];
+                v6 = A[i+k][j+6];
+                v7 = A[i+k][j+7];
+                
+                B[j][i+k] = v0;
+                B[j+1][i+k] = v1;
+                B[j+2][i+k] = v2;
+                B[j+3][i+k] = v3;
+                B[j][i+k+4] = v4;
+                B[j+1][i+k+4] = v5;
+                B[j+2][i+k+4] = v6;
+                B[j+3][i+k+4] = v7;
+            }
+            
+            for (k = 0; k < 4; k++) {
+                v0 = A[i+4+k][j];
+                v1 = A[i+4+k][j+1];
+                v2 = A[i+4+k][j+2];
+                v3 = A[i+4+k][j+3];
+                v4 = A[i+4+k][j+4];
+                v5 = A[i+4+k][j+5];
+                v6 = A[i+4+k][j+6];
+                v7 = A[i+4+k][j+7];
+                
+                B[j+4][i+k] = v0;
+                B[j+5][i+k] = v1;
+                B[j+6][i+k] = v2;
+                B[j+7][i+k] = v3;
+                B[j+4][i+k+4] = v4;
+                B[j+5][i+k+4] = v5;
+                B[j+6][i+k+4] = v6;
+                B[j+7][i+k+4] = v7;
+            }
+        }
+    }
+}
+
+void transpose_61bit(int M, int N, int A[N][M], int B[M][N]) {
+    int i, j, ii, jj;
+    int block_size = 16;
+    
+    for (ii = 0; ii < N; ii += block_size) {
+        for (jj = 0; jj < M; jj += block_size) {
+            for (i = ii; i < ii + block_size && i < N; i++) {
+                for (j = jj; j < jj + block_size && j < M; j++) {
+                    B[j][i] = A[i][j];
+                }
+            }
+        }
+    }
 }
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
+void trans(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, tmp;
 
     for (i = 0; i < N; i++) {
@@ -77,7 +144,6 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             B[j][i] = tmp;
         }
     }    
-
 }
 
 /*
@@ -87,14 +153,12 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
+void registerFunctions() {
     /* Register your solution function */
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
-
 }
 
 /* 
@@ -102,8 +166,7 @@ void registerFunctions()
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
     int i, j;
 
     for (i = 0; i < N; i++) {
@@ -115,4 +178,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
